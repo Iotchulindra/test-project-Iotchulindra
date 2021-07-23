3539fa5da05e804d335b68058114d75741c91975
Iotchulindra <87846587+Iotchulindra@users.noreply.github.com>
2021-07-23 11:30:03 +0545

    Add files via upload

---
+++ employ.c
@@ -0,0 +1,62 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+ 
+typedef struct{
+ 
+    char name[20];
+    int id;
+    int department;
+    int salary;
+ 
+} Employee;
+ 
+int main()
+{
+    int i, n=20;
+ 
+    Employee employees[n];
+ 
+    //Taking each employee detail as input
+ 
+    printf("Enter %d Employee Details \n \n",n);
+    for(i=0; i<n; i++){
+ 
+        printf("Employee %d:- \n",i+1);
+        //Name
+        printf("Name: ");
+        scanf("%s",employees[i].name);
+        //ID
+        printf("Id: ");
+        scanf("%d",&employees[i].id);
+        printf("department:");
+        scanf("%d",employees[i].department);
+        //Salary
+        printf("Salary: ");
+        scanf("%d",&employees[i].salary);
+ 
+        printf("\n");
+    }
+ 
+    //Displaying Employee details
+ 
+    printf("-------------- All Employees Details ---------------\n");
+ 
+    for(i=0; i<n; i++){
+ 
+        printf("Name \t: ");
+        printf("%s \n",employees[i].name);
+ 
+        printf("Id \t: ");
+        printf("%d \n",employees[i].id);
+        printf("department \t",employees[i].department);
+ 
+        printf("Salary \t: ");
+        printf("%d \n",employees[i].salary);
+ 
+        printf("\n");
+    }
+ 
+    return 0;
+ 
+}
